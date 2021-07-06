fb1996c9a93b959be91251ad271bc665ac1fb4cd
Aniket Aryamane <aryamane.aniket@gmail.com>
2021-07-06 10:46:32 +0530

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "bulma-switch/dist/css/bulma-switch.min.css";
+import "bulma/css/bulma.min.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="field is-flex is-justify-content-end">
+    <input
+      id="switchRtlExample"
+      type="checkbox"
+      name="switchRtlExample"
+      class="switch is-rtl"
+      checked="dark"
+      @click="toggleMode"
+    />
+    <label for="switchRtlExample" v-if="dark"> Dark Mode </label>
+    <label for="switchRtlExample" v-else> Light Mode </label>
   </div>
+
+  <section class="section" :class="{ 'has-background-dark': dark }">
+    <div class="columns is-touch is-multiline">
+      <div class="column is-2">
+        <SideNav />
+      </div>
+      <div class="column">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <Nav />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Nav from '@/components/Nav.vue'
+import SideNav from '@/components/SideNav.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Nav,
+    SideNav
+  },
+  setup() {
+    const dark = ref(null)
+    dark.value = true
+    const toggleMode = () => {
+      dark.value = !dark.value
+    }
+    return {
+      dark,
+      toggleMode,
+    }
+  },
 }
+
 </script>
